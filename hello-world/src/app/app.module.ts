afaaeff6223c7a1c0654851ad36521428aa1d60c
vlado <vlado.cavric@etondigital.com>
2021-09-07 16:54:31 +0200

    finished sectio 4

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import {CoursesComponent} from './courses.component';
 import { AuthorsComponent } from './authors/authors.component';
+import { FavoriteComponent } from './favorite/favorite.component';
 
 
 @NgModule({
@@ -11,9 +13,11 @@ import { AuthorsComponent } from './authors/authors.component';
     AppComponent,
     CoursesComponent,
     AuthorsComponent,
+    FavoriteComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ hello-world/src/app/favorite/favorite.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.scss']
+})
+export class FavoriteComponent implements OnInit {
+  favorite = false;
+  title: string;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
